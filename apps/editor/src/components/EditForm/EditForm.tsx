ab6c2b81ef3f5c0f16ad9d3afc3d43e06e73a8a7
dostonabdusattorov <webdeveloperdoston@gmail.com>
2023-09-14 20:40:25 +0500

    added edit button to edit form

--- apps/editor/src/components/EditForm/EditForm.tsx
+++ apps/editor/src/components/EditForm/EditForm.tsx
@@ -1,16 +1,21 @@
 import classNames from 'classnames';
 import Styles from '../Folder/Folder.module.scss';
 import { ChangeEventHandler, FC, FormEventHandler } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
 export interface IEditFormProps {
 	onSubmit: FormEventHandler<HTMLFormElement>;
 	value: string;
 	onChange: ChangeEventHandler<HTMLInputElement>;
 }
-export const EditForm: FC<IEditFormProps> = props => {
+export const EditForm: FC<IEditFormProps> = ({ value, onChange, onSubmit }) => {
 	return (
-		<form className={classNames(Styles.editForm)} onSubmit={props.onSubmit}>
-			<input value={props.value} onChange={props.onChange} autoFocus />
+		<form className={classNames(Styles.editForm)} onSubmit={onSubmit}>
+			<input value={value} onChange={onChange} autoFocus />
+			<button type='submit'>
+				<FontAwesomeIcon icon={faCheckCircle} />
+			</button>
 		</form>
 	);
 };
